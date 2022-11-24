937aae78a64b1be6e987dcdad172a164385e4864
shirazhatim <shirazhatim@gmail.com>
2022-11-24 19:05:44 +0300

    first commit

---
+++ src/components/task-form.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="task-main">
+<h1>Welcome to the Task Program</h1>
+<img id="logo" src="../assets/Taskicon.png" alt="icon" />
+
+<form id="ftask">
+<p>Task Name</p><input type="text" id="taskname"/>
+<p>From Date</p><input type="datetime-local" id="fromdate"/>
+<p>To Date</p><input type="datetime-local" id="todate"/>
+<p>Location</p><input type="text" id="location"/>
+<p>Persons</p><input type="text" id="persons"/>
+<br/><input id="savetask" type="submit"/>
+</form>
+</div>
+
+</template>
+<script>
+</script>
+
+<style>
+#logo{
+    width:5em;
+    height:5em
+}
+#savetask{
+    
+    cursor:pointer;
+}
+#savetask:hover {
+   background-color: lightblue;
+}
+</style>
---
+++ src/views/CreateTask.vue
@@ -0,0 +1,17 @@
+<template>
+<div>hello</div>
+
+<taskform/>
+
+</template>
+
+<script>
+import taskform from '../components/task-form.vue'
+export default{
+    components: {
+        taskform
+    }
+}
+</script>
+
+<style></style>
